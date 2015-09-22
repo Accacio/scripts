b5fcf2908c4d8d5e94fe3f852deb1b23e485964e
Accácio <raccacio2@gmail.com>
2015-09-22 11:02:38 -0300

    eq2transfunv3
    Extraction from .mdl

---
+++ eq2transfunv3.m
@@ -0,0 +1,14 @@
+
+%syms a c d e f g h i j k l m n o p q r s t u v w x y z
+%syms B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+equations=[];
+vars =[];
+
+model= fileread('teste.mdl');
+System=strfind(model,'System {');
+
+System=model(System:end);
+clear model;
+%[A,b]=equationsToMatrix(equations,vars);
+%R=rref(A);
+%Sol=-R(:,end)
