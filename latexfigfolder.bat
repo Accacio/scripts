973317032d65fa10953ac68c74656fbc53286f05
Accácio <raccacio2@gmail.com>
2016-02-05 18:24:39 -0200

    Now latexfigfolder.bat only recognizes pdf png and jpg images

--- latexfigfolder.bat
+++ latexfigfolder.bat
@@ -1,8 +1,8 @@
 @echo off
 set /p id= "Qual o nome da pasta onde estão as figuras?"
-FOR %%f in (.\%id%\*) DO (echo \begin{figure}[h]
+FOR %%f in (.\%id%\*.pdf .\%id%\*.png .\%id%\*.jpg) DO (echo \begin{figure}[H]
 echo 	\begin{center}	
-echo 		\includegraphics[width=8cm]{../%id%/%%~nxf}
+echo 		\includegraphics[width=8cm]{%id%/%%~nxf}
 echo 		\caption{escrevaaquiseucaption}
 echo 		\label{fig:%%~nf}
 echo 	\end{center}
